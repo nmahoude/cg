910555fdcd575e9e95be9853489097403ccfb4ea
NicolasM <nmahoude@gmail.com>
2016-09-20 14:56:20 +0200

    Teads

--- Teads/src/main/java/Solution.java
+++ Teads/src/main/java/Solution.java
@@ -19,7 +19,7 @@ class Solution {
       freeRunners++;
       freeEdges.add(edge);
     }
-    List<Edge> freeEdges = new ArrayList<>();
+    Set<Edge> freeEdges = new HashSet<>();
   }
   static class EdgeRunner {
     boolean active = true;
@@ -30,25 +30,20 @@ class Solution {
         // wait
       } else if (currentNode.freeEdges.size() == 1) {
         Node nextNode;
-        Edge edge = currentNode.freeEdges.get(0);
+        Edge edge = currentNode.freeEdges.iterator().next();
         nextNode = edge.node1 == currentNode ? edge.node2 : edge.node1;
         length++;
         nextNode.freeEdges.remove(edge);
         if (nextNode.freeEdges.size() == 0) {
           if (nextNode.bestRunner != null) {
-            length = Math.max(length, nextNode.bestRunner.length);
             nextNode.bestRunner.desactivate();
-            this.desactivate();
           }
-        }
-        if (nextNode.bestRunner == null ) {
-          nextNode.bestRunner = this;
-        } else if (nextNode.bestRunner.length < this.length) {
-          nextNode.bestRunner.desactivate();
-          nextNode.bestRunner = this;
-        } else {
           this.desactivate();
         }
+        if (nextNode.bestRunner != null ) {
+          nextNode.bestRunner.desactivate();
+        }
+        nextNode.bestRunner = this;
         currentNode = nextNode;
       }
     }
@@ -78,7 +73,6 @@ class Solution {
       Scanner in = new Scanner(System.in);
         int n = in.nextInt(); // the number of adjacency relations
         personsNodes = new Node[2*n];
-        System.err.println("Nodes : "+n);
         for (int i = 0; i < n; i++) {
           int xi = in.nextInt(); // the ID of a person which is adjacent to yi
           int yi = in.nextInt(); // the ID of a person which is adjacent to xi
@@ -92,14 +86,12 @@ class Solution {
             n2 = new Node(yi);
             personsNodes[yi] = n2;
           }
-          System.err.println("Node : "+xi+"<->"+yi);
           new Edge(n1, n2);
         }
         // 1st find the exit nodes
         for (int i=0;i<2*n;i++) {
           Node node = personsNodes[i];
           if (node != null) {
-            System.err.println("node: "+node.index+", edges: "+node.freeEdges.size());
             if (node.freeEdges.size() == 1) {
               EdgeRunner runner = new EdgeRunner();
               runner.currentNode = node;
@@ -109,8 +101,6 @@ class Solution {
           }
         }
 
-        System.err.println("Runners : "+runners.size());
-        
         boolean stillOneActive = false;
         EdgeRunner lastOne = null;
         do {
@@ -128,8 +118,8 @@ class Solution {
         
         // The minimal amount of steps required to completely propagate the advertisement
         System.out.println(lastOne.length);
-
         long time2 = System.currentTimeMillis();
+
         System.err.println(""+(time2-time1)+" ms");
     }
 }
