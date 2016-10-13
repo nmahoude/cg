6348e30214ec8f28bfb650c9402c0dd7a6e63f00
nm128c3n <nmahoude@gmail.com>
2016-10-14 00:15:32 +0200

    TAC

--- multiplayers/theAccountant/src/main/java/theAccountant/Ai.java
+++ multiplayers/theAccountant/src/main/java/theAccountant/Ai.java
@@ -6,78 +6,64 @@ import java.util.List;
 import trigonometry.Point;
 import trigonometry.Vector;
 
-
 public class Ai {
-  Command command ;
+  Command command;
   GameEngine engine = null;
   List<Command> commands = null;
+
   Ai(GameEngine engine) {
     this.engine = engine;
   }
 
-  
   public void doYourStuff() {
     double minDistToMe = Integer.MAX_VALUE;
-    Enemy closestEnemyToMe =  null;
+    Enemy closestEnemyToMe = null;
 
     double minDistToDP = Integer.MAX_VALUE;
-    Enemy closestEnemyToDP =  null;
-    
-    
-    for (Enemy e: engine.enemies) {
+    Enemy closestEnemyToDP = null;
+
+    for (Enemy e : engine.enemies) {
       double dist2 = engine.wolff.p.squareDistance(e.p);
       if (dist2 < minDistToMe) {
         minDistToMe = dist2;
         closestEnemyToMe = e;
       }
-      
+
       Point dpTarget = e.findNearestDataPoint().p;
-      double dist22 = dpTarget.squareDistance(e.p); 
+      double dist22 = dpTarget.squareDistance(e.p);
       if (dist22 < minDistToDP) {
         minDistToDP = dist22;
         closestEnemyToDP = e;
       }
-      
+
     }
-    
+
     Point dpTarget = closestEnemyToMe.findNearestDataPoint().p;
     Point nextPos = closestEnemyToMe.nextPosToTarget(dpTarget);
     double nextDist = nextPos.squareDistance(engine.wolff.p);
-    
     double meToEnemyNearestToDp = engine.wolff.p.squareDistance(closestEnemyToDP.p);
-    
-    if (nextDist < Math.pow(Enemy.ENEMY_WOLFF_RANGE, 2)) {
-      System.err.println("Escape");
-      Vector vec = closestEnemyToMe.p.sub(engine.wolff.p); 
-      command = new Move(new Point(engine.wolff.p.x-vec.vx, engine.wolff.p.y-vec.vy));
-//    } else if (meToEnemyNearestToDp < closestEnemyToMe.distanceToOneShot()*closestEnemyToMe.distanceToOneShot()) {
-//      System.err.println("One shot");
-//      command = new Shoot(closestEnemyToDP);
-//    } else if (meToEnemyNearestToDp > Math.pow(Enemy.ENEMY_WOLFF_RANGE+Wolff.WOLFF_MOVE+Enemy.ENEMY_MOVE, 2)) {
-//      System.err.println("too far");
-//      Vector vec = closestEnemyToDP.p.sub(engine.wolff.p); 
-//      command = new Move(new Point(engine.wolff.p.x+vec.vx, engine.wolff.p.y+vec.vy));
-//    } else if (minDistToMe < closestEnemyToMe.distanceToOneShot()*closestEnemyToMe.distanceToOneShot()) {
-//      System.err.println("One shot");
-//      command = new Shoot(closestEnemyToMe);
+
+    if (minDistToMe < Math.pow(Enemy.ENEMY_WOLFF_RANGE + Enemy.ENEMY_MOVE, 2)) {
+      Vector vec = closestEnemyToMe.p.sub(engine.wolff.p);
+      command = new Move(new Point(engine.wolff.p.x - vec.vx, engine.wolff.p.y - vec.vy));
+    } else if (minDistToMe > Math.pow(Enemy.ENEMY_WOLFF_RANGE + Enemy.ENEMY_MOVE*2 + Wolff.WOLFF_MOVE, 2)) {
+      Vector vec = closestEnemyToMe.p.sub(engine.wolff.p);
+      command = new Move(new Point(engine.wolff.p.x + vec.vx, engine.wolff.p.y + vec.vy));
     } else {
-      System.err.println("Don't know what to do, fire");
       command = new Shoot(closestEnemyToMe);
     }
     // 100%
     /**
-        if (minDist < Math.pow(Enemy.ENEMY_WOLFF_RANGE + Enemy.ENEMY_MOVE, 2)) {
-            Vector vec = closestEnemy.p.sub(engine.wolff.p);
-            command = new Move(new Point(engine.wolff.p.x - vec.vx, engine.wolff.p.y - vec.vy));
-        } else if (minDist > Math.pow(Enemy.ENEMY_WOLFF_RANGE + Enemy.ENEMY_MOVE, 2) * 4) {
-            Vector vec = closestEnemy.p.sub(engine.wolff.p);
-            command = new Move(new Point(engine.wolff.p.x + vec.vx, engine.wolff.p.y + vec.vy));
-        } else {
-            command = new Shoot(closestEnemy);
-        }
+     * if (minDist < Math.pow(Enemy.ENEMY_WOLFF_RANGE + Enemy.ENEMY_MOVE, 2)) {
+     * Vector vec = closestEnemy.p.sub(engine.wolff.p); command = new Move(new
+     * Point(engine.wolff.p.x - vec.vx, engine.wolff.p.y - vec.vy)); } else if
+     * (minDist > Math.pow(Enemy.ENEMY_WOLFF_RANGE + Enemy.ENEMY_MOVE, 2) * 4) {
+     * Vector vec = closestEnemy.p.sub(engine.wolff.p); command = new Move(new
+     * Point(engine.wolff.p.x + vec.vx, engine.wolff.p.y + vec.vy)); } else {
+     * command = new Shoot(closestEnemy); }
      */
   }
-  
+
   public void doYourStuff_old() {
     if (commands == null || commands.isEmpty()) {
       System.err.println("Finding new commands");
@@ -85,11 +71,11 @@ public class Ai {
       if (target == null) {
         System.err.println("Best target is null, escape move");
         commands.clear();
-        commands.add(new Move(new Point(0,0)));
+        commands.add(new Move(new Point(0, 0)));
       } else {
         commands = new StayAndShoot(engine).getCommands(target);
       }
-      System.err.println("Commands: "+commands);
+      System.err.println("Commands: " + commands);
     }
     command = commands.remove(0);
   }
@@ -102,7 +88,7 @@ public class Ai {
       GameEngine copyOfEngine = engine.duplicate();
       Enemy copyOfTarget = copyOfEngine.findEnemyById(e.id);
       commands = new StayAndShoot(copyOfEngine).getCommands(copyOfTarget);
-      System.err.println("Enemy "+e.id+" score will be "+copyOfEngine.getScore());
+      System.err.println("Enemy " + e.id + " score will be " + copyOfEngine.getScore());
       if (copyOfEngine.getScore() > score) {
         best = e;
         score = copyOfEngine.getScore();
@@ -114,9 +100,9 @@ public class Ai {
   private Enemy chooseOneTarget_Closest(List<Enemy> enemies) {
     Enemy best = null;
     int minDist = Integer.MAX_VALUE;
-    
+
     for (Enemy enemy : enemies) {
-      int dist = (int)enemy.p.squareDistance(engine.wolff.p);
+      int dist = (int) enemy.p.squareDistance(engine.wolff.p);
       if (dist < minDist) {
         minDist = dist;
         best = enemy;
