923312aae00a7d1e6344b6e5b86fe824c0cac3b4
nm128c3n <nmahoude@gmail.com>
2016-12-09 23:20:35 +0100

    Sotk2

--- veryHard/ShadowsOfTheKnight2/src/main/java/Player.java
+++ veryHard/ShadowsOfTheKnight2/src/main/java/Player.java
@@ -6,197 +6,86 @@ import java.io.*;
 import java.math.*;
 
 class Player {
-    static class P {
-      int x;
-      int y;
-      
-      P() {
-      }
-      P(int x, int y) {
-        this.x = x;
-        this.y = y;
-      }
-    }
+  private static final String UNKNOWN = "UNKNWON";
+  private static final String COLDER = "COLDER";
+  private static final String WARMER = "WARMER";
+  private static final String SAME = "SAME";
+
   
-    static class Rectangle {
-        int x1,y1;
-        int x2,y2;
-        
-        Rectangle(int x1, int y1, int x2, int y2) {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-        }
-        
-        int[] middle() {
-            return new int[] { x1 + (x2-x1)/2,  y1+(y2-y1)/2};
-        }
-        void debug() {
-            System.err.println("x1,y1: "+x1+","+y1);
-            System.err.println("  x2,y2: "+x2+","+y2);
-        }
-    }
+  public static void main(String args[]) {
+    Scanner in = new Scanner(System.in);
+    int W = in.nextInt(); // width of the building.
+    int H = in.nextInt(); // height of the building.
+    int N = in.nextInt(); // maximum number of turns before game over.
 
-    enum Cut {
-      VERTICAL,
-      HORIZONTAL,
-      IGNORE,
-    }
-    private static final String UNKNOWN = "UNKNWON";
-    private static final String COLDER = "COLDER";
-    private static final String WARMER = "WARMER";
-    private static final String SAME = "SAME";
+    int L = 0;
+    int R = W-1;
+    int oldL = L;
+    int oldR = R;
     
-    static Cut nextCut;
-    private static Cut lastCut;
+    int Bx = in.nextInt();
+    int oldBx = Bx;
+    int nextBx = 0;
+    int By = in.nextInt();
 
-    static P lastPos = new P();
-    static P currentPos = new P();
-    static P projectedPos = new P();
-    private static boolean ignoreNextCut;
-    private static boolean ignoreLastNextCut;
-
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int W = in.nextInt(); // width of the building.
-        int H = in.nextInt(); // height of the building.
-        int N = in.nextInt(); // maximum number of turns before game over.
-
-        lastPos.x = currentPos.x;
-        lastPos.y = currentPos.y;
-        currentPos.x = in.nextInt();
-        currentPos.y = in.nextInt();
-        
-        int originX = 0;
-        int originY = 0;
-        int sizeX = W;
-        int sizeY = H;
-        int lastCutY = 0;
-        int lastCutX = 0;
-        
-        
-        // game loop
-        lastCut = null;
-        nextCut = Cut.HORIZONTAL;
-        Rectangle rect = new Rectangle(0,0,W-1,H-1);
-        while (true) {
-            String bombTemp = in.next(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
-            System.err.println("Ignore next is "+ignoreNextCut);
-            System.err.println("bombTemp: "+bombTemp);
-            System.err.println("old rect:");
-            rect.debug();
-            System.err.println("Batman(curr): "+currentPos.x+","+currentPos.y);
-
-            
-            int deltax = lastPos.x+projectedPos.x % 2 == 0 ? 0 : 1;
-            int deltay = lastPos.y+projectedPos.y % 2 == 0 ? 0 : 1;
-            if (ignoreNextCut) {
-              ignoreNextCut = false;
-              ignoreLastNextCut = true;
-            } else if (bombTemp.equals(UNKNOWN)) {
-              
-            } else if (bombTemp.equals(WARMER)) {
-              
-              if (lastCut == Cut.VERTICAL) {
-                if (projectedPos.x == rect.x1) {
-                  rect.x2 = (lastPos.x+projectedPos.x+deltax) / 2;
-                } else {
-                  rect.x1 = (lastPos.x+projectedPos.x+deltax) / 2;
-                }
-              } else if (lastCut == Cut.HORIZONTAL) {
-                if (projectedPos.y == rect.y1) {
-                  rect.y2 = (lastPos.y+projectedPos.y+deltay) / 2;
-                } else {
-                  rect.y1 = (lastPos.y+projectedPos.y+deltay) / 2;
-                }
-              }
-            } else if (bombTemp.equals(COLDER)) {
-              ignoreNextCut = true;
-              if (lastCut == Cut.VERTICAL) {
-                if (projectedPos.x == rect.x1) {
-                  rect.x1 = (lastPos.x+projectedPos.x+deltax) / 2;
-                  currentPos.x = rect.x1;
-                } else {
-                  rect.x2 = (lastPos.x+projectedPos.x+deltax) / 2;
-                  currentPos.x = rect.x2;
-                }
-              } else if (lastCut == Cut.HORIZONTAL) {
-                if (projectedPos.y == rect.y1) {
-                  rect.y1 = (lastPos.y+projectedPos.y+deltay) / 2;
-                  currentPos.y = rect.y1;
-                } else {
-                  rect.y2 = (lastPos.y+projectedPos.y+deltay) / 2;
-                  currentPos.y = rect.y2;
-                }
-              }
-              nextCut = lastCut;
-            } else if (bombTemp.equals(SAME)) {
-              if (lastCut == Cut.HORIZONTAL) {
-                rect.y1 = (lastPos.y+projectedPos.y+ deltay) / 2;
-                rect.y2 = rect.y1;
-                nextCut = Cut.VERTICAL;
-                currentPos.y = rect.y1;
-                System.err.println("***LOCK Y***");
-              } else {
-                rect.x1 = (lastPos.x+projectedPos.x+deltax) / 2;
-                rect.x2 = rect.x1;
-                currentPos.x = rect.x1;
-                nextCut = Cut.HORIZONTAL;
-                System.err.println("***LOCK X***");
-              }
-            }
-            
-            System.err.println("New rect:");
-            rect.debug();
-
-            projectedPos.x = currentPos.x;
-            projectedPos.y = currentPos.y;
-            if (ignoreLastNextCut) {
-              ignoreLastNextCut = false;
-              if (nextCut == Cut.VERTICAL) {
-                projectedPos.y += 1;
-              } else {
-                projectedPos.x += 1;
-              }
-            } else if (ignoreNextCut) {
-              if (nextCut == Cut.VERTICAL) {
-                projectedPos.x = rect.middle()[0];
-              } else {
-                projectedPos.y = rect.middle()[1];
-              }
-            } else 
-            if (nextCut == Cut.VERTICAL) {
-              // find best way to cut
-              if (currentPos.x - rect.x1 > rect.x2 - currentPos.x) {
-                projectedPos.x = rect.x1;
-              } else {
-                projectedPos.x = rect.x2;
-              }
-            } else if (nextCut == Cut.HORIZONTAL) {
-              if (currentPos.y - rect.y1 > rect.y2 - currentPos.y) {
-                projectedPos.y = rect.y1;
-              } else {
-                projectedPos.y = rect.y2;
-              }
-            }
-              
-            System.err.println("Batman(proj): "+projectedPos.x+","+projectedPos.y);
-
-
-            lastCut = nextCut;
-            if (lastCut == Cut.HORIZONTAL && rect.x1 != rect.x2) {
-              nextCut = Cut.VERTICAL;
-            }
-            if (lastCut == Cut.VERTICAL && rect.y1 != rect.y2) {
-              nextCut = Cut.HORIZONTAL;
-            }
-
-            lastPos.x = currentPos.x;
-            lastPos.y = currentPos.y;
-            currentPos.x = projectedPos.x;
-            currentPos.y = projectedPos.y;
-            System.out.println(""+projectedPos.x+" "+projectedPos.y);
+    int lastMoveType = 0; // 0 in between
+                      // 1 > R
+                      // 2 < L
+    
+    while (true) {
+      boolean bypass = false;
+      System.err.println("L="+L+" , R="+R+" , batX="+Bx);
+      String bombTemp = in.next();
+      
+      if (UNKNOWN.equals(bombTemp)) {
+        // first turn, nothing to do
+      } else if (WARMER.equals(bombTemp)) {
+        if (lastMoveType == 0) {
+          L = L + (R-L) / 2 +1;
+        } else if (lastMoveType == 1) {
+          R = oldBx- (oldBx-L)/2;
+        } else { // 2
+          L = oldBx + (R-oldBx)/2;
         }
+      } else if (COLDER.equals(bombTemp)) {
+        if (lastMoveType == 0) {
+          R = L + (R-L) / 2;
+        } else if (lastMoveType == 1) {
+          L = oldBx- (oldBx-L)/2;
+        } else { // 2
+          R = oldBx + (R-oldBx)/2;
+        }
+      } else if (SAME.equals(bombTemp)) {
+        R = (R+L)/2;
+        L = R;
+        nextBx = L;
+        bypass = true;
+      }
+      
+      if (L == R) {
+        System.err.println(" trouve");
+        continue;
+      }
+      
+      if (!bypass) {
+        if (L <= Bx && Bx <= R) {
+          nextBx = R - (Bx-L);
+          if (nextBx == Bx) nextBx = Bx+1;
+          lastMoveType = 0;
+        } else {
+          if (Bx > R) {
+            nextBx = L;
+            lastMoveType = 1;
+          } else {
+            nextBx = R;
+            lastMoveType = 2;
+          }
+        }
+      }
+      
+      System.out.println("" + nextBx + " " + By);
+      oldBx = Bx;
+      Bx = nextBx;
     }
+  }
 }
