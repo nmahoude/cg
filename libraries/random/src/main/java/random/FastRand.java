e2e9f37481208ca3678ba131db52c464e323a3be
NicolasM <nmahoude@gmail.com>
2016-12-09 09:58:09 +0100

    fast rand library

---
+++ libraries/random/src/main/java/random/FastRand.java
@@ -0,0 +1,32 @@
+package random;
+
+
+public class FastRand {
+  int g_seed;
+  
+  public FastRand(int seed) {
+    g_seed = seed;
+  }
+  public void fast_srand(int seed) {
+    //Seed the generator
+    g_seed = seed;
+  }
+  public int fastrand() {
+    //fastrand routine returns one integer, similar output value range as C lib.
+    g_seed = (214013*g_seed+2531011);
+    return (g_seed>>16)&0x7FFF;
+  }
+  public int fastRandInt(int maxSize) {
+    return fastrand() % maxSize;
+  }
+  public int fastRandInt(int a, int b) {
+    return(a + fastRandInt(b - a));
+  }
+  public double fastRandDouble() {
+    return (fastrand()) / 0x7FFF;
+  }
+  public double fastRandDouble(double a, double b) {
+    return a + ((fastrand()) / 0x7FFF)*(b-a);
+  }
+
+}
