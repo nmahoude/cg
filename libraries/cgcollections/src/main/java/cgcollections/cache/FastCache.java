ac91396dcf715d5bd0b7e24583432ac00b1975ea
NicolasM <nmahoude@gmail.com>
2017-04-25 21:04:59 +0200

    Add fast cache to cgcollections

--- libraries/cgcollections/src/main/java/cgcollections/cache/FastCache.java
+++ libraries/cgcollections/src/main/java/cgcollections/cache/FastCache.java
@@ -8,9 +8,23 @@ public class FastCache <T> {
   int currentFreeIndex;
   
   @SuppressWarnings("unchecked")
-  public FastCache(Class<T> c,int size) {
+  /**
+   * T need an empty constructor
+   * @param c
+   * @param size
+   * @throws InstantiationException
+   * @throws IllegalAccessException
+   */
+  public FastCache(Class<T> c,int size)  {
     this.size = size;
     this.elements = (T[]) Array.newInstance(c, size);
+    try {
+      for (int i=0;i<size;i++) {
+        elements[i] = c.newInstance();
+      }
+    } catch (InstantiationException | IllegalAccessException e) {
+      throw new RuntimeException("T needs an empty constructor");
+    }
     currentFreeIndex = size;
   }
   
