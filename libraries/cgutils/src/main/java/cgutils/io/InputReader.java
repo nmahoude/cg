008c0d436e62d5daa44986f9bf0b332877684f9c
NicolasM <nmahoude@gmail.com>
2017-04-28 09:51:08 +0200

    update librairies CG

---
+++ libraries/cgutils/src/main/java/cgutils/io/InputReader.java
@@ -0,0 +1,85 @@
+package cgutils.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputReader {
+  private InputStream stream;
+  private byte[] buf = new byte[1024];
+
+  private int curChar;
+
+  private int numChars;
+
+  public InputReader(InputStream stream) {
+      this.stream = stream;
+  }
+
+  public int read() {
+      if (numChars == -1)
+          throw new RuntimeException();
+      if (curChar >= numChars) {
+          curChar = 0;
+          try {
+              numChars = stream.read(buf);
+          } catch (IOException e) {
+              throw new RuntimeException();
+          }
+          if (numChars <= 0)
+              return -1;
+      }
+      return buf[curChar++];
+  }
+
+  public String nextString() {
+      final StringBuilder stringBuilder = new StringBuilder();
+      int c = read();
+      while (isSpaceChar(c))
+          c = read();
+      do {
+          stringBuilder.append(c);
+          c = read();
+      } while (!isSpaceChar(c));
+      return stringBuilder.toString();
+  }
+
+  public int nextInt() {
+      int c = read();
+      while (isSpaceChar(c))
+          c = read();
+      int sgn = 1;
+      if (c == '-') {
+          sgn = -1;
+          c = read();
+      }
+      int res = 0;
+      do {
+          res *= 10;
+          res += c - '0';
+          c = read();
+      } while (!isSpaceChar(c));
+      return res * sgn;
+  }
+
+  public long nextLong() {
+      int c = read();
+      while (isSpaceChar(c))
+          c = read();
+      int sgn = 1;
+      if (c == '-') {
+          sgn = -1;
+          c = read();
+      }
+      long res = 0;
+      do {
+          res *= 10;
+          res += c - '0';
+          c = read();
+      } while (!isSpaceChar(c));
+      return res * sgn;
+  }
+
+  public boolean isSpaceChar(int c) {
+      return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+  }
+}
