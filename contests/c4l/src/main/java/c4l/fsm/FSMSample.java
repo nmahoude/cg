b18a90a9b66ccbb68d1dff8ac01c3fdbecd2b1a1
NicolasM <nmahoude@gmail.com>
2017-05-16 01:37:05 +0200

    c4l - change sample retrieval function  (silver 12)

--- contests/c4l/src/main/java/c4l/fsm/FSMSample.java
+++ contests/c4l/src/main/java/c4l/fsm/FSMSample.java
@@ -20,49 +20,30 @@ public class FSMSample extends FSMNode {
   
   
   private void getSomeSamples() {
-
-    if (me.totalCarried > 8) {
-      // TODO play on the xp ? maybe rank1 is too absolute
-      fsm.connect(1, "Full molecule, on prend des rank 1 pour debloquer");
+    if (me.totalExpertise< 6) {
+      fsm.connect(1, "go fast at start (copied on Agade strategy");
       return;
     }
-    
-    if (me.totalExpertise < 5) {
-      fsm.connect(2, "Always get 2, not enough XP");
-      return;
-    } else {
-//      if (state.robots[1].score - me.score >= 20) {
-//        fsm.connect(3, "We are behind, take some risks");
-//        return;
-//      }
-      System.err.println("rank 2 or 3, I don't care");
-      if (Player.rand.nextBoolean()) {
-        fsm.connect(2, "Rand say take rank 2");
+
+    // TODO check in the cloud, if there is a lot of rank2 and we can't do them it means something
+    if (me.totalCarried > 8) {
+      // TODO play on the xp ? maybe rank1 is too absolute
+      if (me.totalExpertise < 12) {
+        fsm.connect(1, "Full molecule, on prend des rank 1 pour debloquer");
         return;
       } else {
-        fsm.connect(3, "Rand say take rank 3");
+        fsm.connect(2, "Full molecule, on prend des rank 2 pour debloquer parce que xp suffisante");
         return;
       }
     }
-
-//    if (me.totalExpertise < 0) {
-//      System.err.println("Noob, get a 1");
-//      fsm.connect(1);
-//      return;
-//    } else if (me.totalExpertise < 4) {
-//      System.err.println("Not enough expertise, get a 2");
-//      fsm.connect(2);
-//      return;
-//    } else {
-//      System.err.println("rank 2 or 3, I don't care");
-//      if (Player.rand.nextBoolean()) {
-//        fsm.connect(2);
-//        return;
-//      } else {
-//        fsm.connect(3);
-//        return;
-//      }
-//    }
+    
+    if (me.totalExpertise < 12) {
+      fsm.connect(2, "Always get 2, not enough XP");
+      return;
+    } else {
+        fsm.connect(3, "Rand say take rank 3");
+        return;
+    }
   }
 
 
