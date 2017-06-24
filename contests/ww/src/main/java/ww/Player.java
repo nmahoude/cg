101ead6ba694f79c8ff12cf8f8f8c19b8624968c
nmahoude <nmahoude@gmail.com>
2017-06-24 13:29:26 +0200

    WW - B50 - Modification eval heuristic push & move

--- contests/ww/src/main/java/ww/Player.java
+++ contests/ww/src/main/java/ww/Player.java
@@ -26,6 +26,7 @@ public class Player {
 
         bestScore[i] = Integer.MIN_VALUE;
         
+        int actionsLeft = 0; // count possible actions for the agent, if too few, we force this agent to move (boost his score)
         for (Dir dir : Dir.values()) {
           int dirX = unitX+dir.dx;
           int dirY = unitY+dir.dy;
@@ -33,10 +34,15 @@ public class Player {
           int deltaHeight = state.getHeight(dirX,dirY) - state.getHeight(unitX,unitY);
           if (deltaHeight > 1 ) continue;
         
-          int moveScore = 20*state.getHeight(dirX, dirY) + (state.getHeight(dirX, dirY) == 3 ? 1000 : 0);
           
+          boolean onePushOrBuild = false;
           if (state.isOccupied(i, dirX, dirY)) {
             if (state.isFriendly(i, dirX, dirY)) continue;
+            
+            // Push&Build action score calculation :
+            int moveScore = 0
+                // juste get our current height as there is no score++ on push 
+                + 20*state.getHeight(unitX, unitY); 
             
             for (Dir push : dir.pushDirections()) {
               int pushedX = dirX+push.dx;
@@ -46,8 +52,14 @@ public class Player {
               
               int deltaY = state.getHeight(dirX, dirY)-state.getHeight(pushedX, pushedY);
               if (deltaY < 0) continue;
+        
+              onePushOrBuild = true;
+              int pushScore = 0
+                  // just under 1 floor up for me so I prefer climbing, but 2 stairs fall is better
+                  + 15 * deltaY
+                  // If opp is on lvl 3 and we push it <= than lvl 1, it's a very good move
+                  + (state.getHeight(dirX, dirY) == 3 ? 1000*Math.max(deltaY-1,0) : 0); 
               
-              int pushScore = 15 * deltaY; // just under 1 floor up for me so I prefer climbing, but 2 stairs fall is better
               if ( pushScore > bestScore[i]) {
                 bestScore[i] = pushScore;
                 best[i] = "PUSH&BUILD "+i+" "+dir.toString()+" "+push.toString();
@@ -56,22 +68,42 @@ public class Player {
               }
             }
           } else {
+            // Move&Build action score calculation :
+            int moveScore = 0
+                // our next height
+                + 20*state.getHeight(dirX, dirY) 
+                // if we go on a lvl 3 height next, it's a big bonus (score 1 point)
+                + (state.getHeight(dirX, dirY) == 3 ? 1000 : 0);
+
             for (Dir dirBlock : Dir.values()) {
               int blockX = dirX+dirBlock.dx;
               int blockY = dirY+dirBlock.dy;
               if (!state.isValid(blockX, blockY)) continue;
               if (state.isOccupied(i, blockX, blockY)) continue;
               
-              int buildScore = state.getHeight(blockX,blockY) + (state.getHeight(blockX,blockY) == 3 ? -5 : 0);
+              int deltaY = state.getHeight(blockX,blockY) + 1 - state.getHeight(dirX, dirY);
+              onePushOrBuild=true;
+              int buildScore = 0
+                  // height of the next block we put
+                  + state.getHeight(blockX,blockY) 
+                  // Malus if we go to height 4
+                  + (state.getHeight(blockX,blockY) == 3 ? -5 : 0)
+                  // Malus if the block is higher than our next position
+                  - (deltaY > 1 ? 100 : 0);
               
               if (moveScore + buildScore > bestScore[i]) {
                 bestScore[i] = moveScore + buildScore;
                 best[i]="MOVE&BUILD "+i+" "+dir.toString()+" "+dirBlock.toString();
-                System.err.println("for "+i+" pos = "+unitX+" "+unitY+" best is "+best[i]);
-                System.err.println("Scores are  : move = "+moveScore+" , build= "+buildScore);
+                //System.err.println("for "+i+" pos = "+unitX+" "+unitY+" best is "+best[i]);
+                //System.err.println("Scores are  : move = "+moveScore+" , build= "+buildScore);
               }
             }
           }
+          if (onePushOrBuild) actionsLeft++;
+        }
+        System.err.println("Agent "+i+" has "+actionsLeft+" actions left");
+        if (actionsLeft >0 && actionsLeft < 3) {
+          bestScore[i] += 1_000_000; // force player to move
         }
       }
 
