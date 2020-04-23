063d63b88d99e477b251d54bca1330dfabbc615c
MAHOUDEAUX <nicolas.mahoudeaux@capgemini.com>
2020-04-23 17:58:56 +0200

    cleaning uttt

--- multiplayers/uttt_java/src/test/java/uttt/mcts/Perf.java
+++ multiplayers/uttt_java/src/test/java/uttt/mcts/Perf.java
@@ -1,30 +1,3 @@
-<<<<<<< Updated upstream
-package uttt.mcts;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import uttt.Player;
-
-public class Perf {
-  @Before
-  public void setup() {
-    NodeCache.init();
-  }
- 
-  public static void main(String[] args) {
-    Player.DEBUG = false;
-    
-    for (int i=0;i<400;i++) {
-      NodeCache.resetCache();
-      MCTS mcts = new MCTS();
-      Player.start = System.currentTimeMillis();
-      mcts.think();
-    }
-//    mcts.output();
-  }
-}
-=======
 package uttt.mcts;
 
 import org.junit.Before;
@@ -48,7 +21,6 @@ public class Perf {
       Player.start = System.currentTimeMillis();
       mcts.think();
     }
-//    mcts.output();
   }
 }
->>>>>>> Stashed changes
+
