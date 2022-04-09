a35d4d905a9a048dad971fa3a876c754381db9d5
nmahoude <nmahoude@gmail.com>
2022-04-09 09:53:53 +0200

    othello - prepare transformation of state from int[][] to long

---
+++ multiplayers/othello/src/test/java/othello/StateTest.java
@@ -0,0 +1,12 @@
+package othello;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class StateTest {
+
+  @Test
+  public void alwaysTure() throws Exception {
+    Assertions.assertThat(true).isTrue();
+  }
+}
