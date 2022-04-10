c80250eb34bc14aa19df5cf923045465dfed1ab0
nmahoude <nmahoude@gmail.com>
2022-04-10 17:39:16 +0200

    libs/logger - upgrade logger

--- libraries/cgutils/src/main/java/cgutils/logger/Level.java
+++ libraries/cgutils/src/main/java/cgutils/logger/Level.java
@@ -1,8 +1,20 @@
 package cgutils.logger;
 
 public enum Level {
-	NONE,
-	ERROR,
-	INFO,
-	DEBUG;
+	NONE(0),
+	ERROR(1),
+	INFO(2),
+	DEBUG(3);
+
+  private int priority;
+
+  Level(int priority) {
+    this.priority = priority;
+  }
+
+  public boolean isInferiorOrEqualTo(Level level) {
+    return this.priority <= level.priority;
+  }
+  
+  
 }
--- libraries/cgutils/src/main/java/cgutils/logger/Logger.java
+++ libraries/cgutils/src/main/java/cgutils/logger/Logger.java
@@ -1,11 +1,15 @@
 package cgutils.logger;
 
+import java.util.Date;
 
 public class Logger {
 	private static Level globalLevel = Level.DEBUG;
 
 	private final Level level;
   private final String logName;
+
+  private boolean appendLogName = false;
+  private boolean appendTime = false;
 
   private Logger(String name, Level levelSpecific) {
     this.logName = name;
@@ -30,33 +34,77 @@ public class Logger {
     return ret;
   }
 
+  public static class LoggerBuilder {
+    private Level level;
+    private String name;
+    private boolean appendTime;
+    private boolean appendLogName;
+
+    public LoggerBuilder(String name) {
+      this.name = name;
+    }
+
+    public LoggerBuilder withName(String name) {
+      this.name = name;
+      return this;
+    }
+    public LoggerBuilder withLevel(Level level) {
+      this.level = level;
+      return this;
+    }
+    
+    public LoggerBuilder withTimeAppended() {
+      this.appendTime = true;
+      return this;
+    }
+    
+    public LoggerBuilder withNameAppended() {
+      this.appendLogName = true;
+      return this;
+    }
+    
+    public Logger build() {
+      Logger logger = new Logger(name, level); 
+      logger.appendTime = this.appendTime;
+      logger.appendLogName = this.appendLogName;
+      return logger;
+    }
+  }
+  
+  
   public void error(Object... s) {
-    print("[ERROR]", s);
+    if (getLoggerLevel().isInferiorOrEqualTo(Level.INFO)) {
+      print("[ERROR]", s);
+    }
   }
 
   public void info(Object... s) {
-    if (getLevel() == Level.DEBUG || getLevel() == Level.INFO) {
+    if (getLoggerLevel().isInferiorOrEqualTo(Level.INFO)) {
       print("[INFO ]",s);
     }
   }
 
   public void debug(Object... s) {
-    if (getLevel() == Level.DEBUG) {
+    if (getLoggerLevel().isInferiorOrEqualTo(Level.DEBUG)) {
       print("[DEBUG]", s);
     }
   }
 
   private static StringBuilder sb = new StringBuilder(5000);
 
-  public void print(Object... s) {
-    if (getLevel() == Level.NONE) {
-      return;
-    }
+  protected void print(Object... s) {
     sb.delete(0, sb.length());
-//    sb.append(Timer.getInstance().getTime());
-//    sb.append(' ');
-    sb.append(logName);
-    sb.append(" : ");
+    
+    if (appendTime ) {
+      sb.append(new Date());
+      sb.append(' ');
+    }
+    
+    if (appendLogName ) {
+      sb.append(logName);
+      sb.append(" : ");
+    }
+    
     int i = 0;
     for (Object o : s) {
       sb.append(o == null ? "null" : o.toString());
@@ -64,15 +112,23 @@ public class Logger {
         sb.append(' ');
       }
     }
+    outputSb();
+  }
+
+  protected void outputSb() {
     System.err.println(sb);
   }
 
-  public void disableAllLogs() {
+  public static void disableAllLogs() {
   	globalLevel = Level.NONE;
   }
   
-  private Level getLevel() {
-    if (level == null || globalLevel == Level.NONE) {
+  public static void setGlobalLevel(Level level) {
+    globalLevel = level;
+  }
+
+  private Level getLoggerLevel() {
+    if (level == null || level.isInferiorOrEqualTo(globalLevel)) {
       return globalLevel;
     }
     return level;
