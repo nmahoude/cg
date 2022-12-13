5094a4830bba307917741360d6b346336a703c0f
nmahoude <nmahoude@gmail.com>
2022-12-13 15:14:50 +0100

    new option pane option : easily declare checkboxes

--- libraries/cgfx/src/main/java/cgfx/components/GameOptionPane.java
+++ libraries/cgfx/src/main/java/cgfx/components/GameOptionPane.java
@@ -1,11 +1,37 @@
 package cgfx.components;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Observable;
 
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.VBox;
 
 public abstract class GameOptionPane extends VBox{
 
+	private final Map<String, CheckBox> checkBoxes = new HashMap<>();
+	
+	public void addCheckBox(String name, String displayName, boolean defaultValue) {
+		CheckBox cb = new CheckBox(displayName);
+		cb.setSelected(defaultValue);
+		cb.selectedProperty().addListener(event -> {
+			observable.notifyObservers();
+		});
+		checkBoxes.put(name, cb);
+		this.getChildren().add(cb);
+	}
+	
+	public boolean isSelected(String name) {
+		CheckBox cb = checkBoxes.get(name);
+		if (cb == null) {
+			// TODO log
+			return false;
+		} else {
+			return cb.isSelected();
+		}
+	}
+
+	
   protected static class MyObservable extends Observable {
     @Override
     public void notifyObservers() {
